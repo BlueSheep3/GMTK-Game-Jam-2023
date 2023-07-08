d4e5e86a7c2ea6042ec1498aed3ded95c4f17a49
Per6 <per.daude@t-online.de>
2023-07-08 10:44:15 +0200

    tileplacer

--- Assets/Scripts/TilePlacer.cs
+++ Assets/Scripts/TilePlacer.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using UnityEngine.UI;
 
 class TilePlacer : MonoBehaviour
 {
 	public Camera mainCamera;
-	public CustomRuleTile[] allTileOptions;
 	public Tilemap tilemap;
+	public GameObject canvas;
+	public CustomRuleTile[] allTileOptions;
 	public int[] tileAmounts;
 	public GameObject tileCounter;
 	List<TMPro.TextMeshProUGUI> tileCounterCounts = new();
@@ -19,20 +21,23 @@ class TilePlacer : MonoBehaviour
 		if(allTileOptions.Length != tileAmounts.Length)
 			Debug.LogError("Tile options and amounts do not match");
 		int counter = 0;
-		for(int i = tileAmounts.Length; i > 0; i--)
-			if(tileAmounts[i] >= 0) {
+		for(int i = tileAmounts.Length - 1; i >= 0; i--)
+			if(tileAmounts[i] > 0) {
 				currentTile = i;
 				counter++;
 			}
 		if(currentTile == -1)
 			Debug.LogError("No tiles in the list");
-		for(int i = 0; i < tileAmounts.Length; i++)
+		int counter2 = 0;
+		for(int i = 0; i < tileAmounts.Length; i++) {
 			if(tileAmounts[i] > 0) {
-				Transform tileCounterObj = Instantiate(tileCounter, new Vector3(10 * counter - 5 * i, 0, 0), Quaternion.identity).transform.GetChild(0);
+				Transform tileCounterObj = Instantiate(tileCounter, canvas.transform).transform;
+				tileCounterObj.localPosition += new Vector3((counter2++ - counter/2f) * tileCounter.GetComponent<RectTransform>().rect.width, -250, 0);
 				tileCounterObj.GetChild(2).GetComponent<TMPro.TextMeshProUGUI>().text = tileAmounts[i].ToString();
-				tileCounterObj.GetChild(1).GetComponent<SpriteRenderer>().sprite = allTileOptions[i].m_DefaultSprite;
+				tileCounterObj.GetChild(1).GetComponent<Image>().sprite = allTileOptions[i].m_DefaultSprite;
 				tileCounterCounts.Add(tileCounterObj.GetChild(2).GetComponent<TMPro.TextMeshProUGUI>());
 			}
+		}
 	}
     
 
@@ -48,34 +53,77 @@ class TilePlacer : MonoBehaviour
 			if(CanPlaceTile(currentTile, cellPos))
 				PlaceTile(currentTile, cellPos);
         }
+		if(Input.GetKeyDown(KeyCode.Alpha1))
+			SetCurrentTile(0);
+		if(Input.GetKeyDown(KeyCode.Alpha2))
+			SetCurrentTile(1);
+		if(Input.GetKeyDown(KeyCode.Alpha3))
+			SetCurrentTile(2);
+		if(Input.GetKeyDown(KeyCode.Alpha4))
+			SetCurrentTile(3);
+		if(Input.GetKeyDown(KeyCode.Alpha5))
+			SetCurrentTile(4);
+		if(Input.GetKeyDown(KeyCode.Alpha6))
+			SetCurrentTile(5);
+		if(Input.GetKeyDown(KeyCode.Alpha7))
+			SetCurrentTile(6);
+		if(Input.GetKeyDown(KeyCode.Alpha8))
+			SetCurrentTile(7);
+		if(Input.GetKeyDown(KeyCode.Alpha9))
+			SetCurrentTile(8);
     }
+
+	void SetCurrentTile(int index) {
+		int p = -1;
+		for(int i = 0; i < tileAmounts.Length; i++) {
+			if(tileAmounts[i] > 0) {
+				p++;
+			}
+			if(p == index) {
+				currentTile = i;
+				return;
+			}
+		}
+	}
 
 	void PlaceTile(int index, Vector3Int position)
 	{
 		tilemap.SetTile(position, allTileOptions[index]);
-		tileAmounts[index]--;
-		UpdateOneTileCounter();
+		SubtractFromTileCounter(index);
 	}
 
-	void UpdateOneTileCounter() {
+	void SubtractFromTileCounter(int index) {
 		int counter = 0;
-		for(int i = 0; i < tileAmounts.Length; i++)
+		for(int i = 0; i < tileAmounts.Length; i++) {
 			if(tileAmounts[i] > 0) {
-				if(tileAmounts[i].ToString() != tileCounterCounts[counter].text) {
+				if(i == index) {
+					tileAmounts[index]--;
 					tileCounterCounts[counter].text = tileAmounts[i].ToString();
+					if(tileAmounts[index] == 0) {
+						tileCounterCounts.RemoveAt(counter);
+						currentTile = GetFirstTileIndex();
+					}
 					return;
 				}
 				counter++;
 			}
+		}
+	}
+	
+	int GetFirstTileIndex() {
+		for(int i = 0; i < tileAmounts.Length; i++) {
+			if(tileAmounts[i] > 0)
+				return i;
+		}
+		return 0;
 	}
 
 	bool CanPlaceTile(int index, Vector3Int position) {
 		if(tileAmounts[index] <= 0)
 			return false;
 		if(tilemap.HasTile(position))
-			return false;
-		if(tilemap.GetColliderType(position) == Tile.ColliderType.None)
-			return false;
+			if(tilemap.GetColliderType(position) != Tile.ColliderType.None)
+				return false;
 		return true;
 	}
 }
