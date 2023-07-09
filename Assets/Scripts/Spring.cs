e866f9904c30157918551230669abbab6ab006c5
Per6 <per.daude@t-online.de>
2023-07-09 11:27:56 +0200

    spring doesn't like the game objects :(

--- Assets/Scripts/Spring.cs
+++ Assets/Scripts/Spring.cs
@@ -5,13 +5,19 @@ class Spring : MonoBehaviour
 {
 	public UnityEngine.RuntimeAnimatorController anim;
 	
+	void Start() {
+		transform.GetComponent<Animator>().speed = 0.7f;
+	}
 
-	void OnCollisionEnter2D(Collision2D collision) {
+	void OnTriggerEnter2D(Collider2D collision) {
 		Debug.Log("spring");
 		if (collision.gameObject.tag != "Player")
 			return;
-		Vector2 force = transform.up * 5;
-		collision.gameObject.GetComponent<Rigidbody2D>().AddForce(force);
+		Vector2 force = transform.up * 20;
+		if(force.x != 0)
+			Player.inst.rb.velocity = new Vector2(force.x, Player.inst.rb.velocity.y);
+		if(force.y != 0)
+			Player.inst.rb.velocity = new Vector2(Player.inst.rb.velocity.x, force.y);
 		SwitchAnimation();
 		transform.GetComponent<BoxCollider2D>().enabled = false;
 		Invoke("EnableCollider", 0.5f);
